4772bef342850e15df2a6197557cf556a002091e
Warut Surapat <wsurapat@gilt.jp>
2019-03-04 13:41:42 +0900

    Fix count_inbetween_zero that miss count leading zero.

--- water_trap/ruby/another_trap.rb
+++ water_trap/ruby/another_trap.rb
@@ -4,14 +4,20 @@ class AnotherTrap
     arr.inject({stack: [], pending_zero_count: 0, zero_count: 0}) do |acc, i|
       stack = acc[:stack]
 
-      if (stack.length == 0)
-        if i == 1
-          stack.push(i)
-        end
+      if (stack.length == 0) && (i == 1)
+        stack.push(i)
       else
+        if (stack.last == 1) && (i == 1)
+          acc[:zero_count] = acc[:zero_count] + acc[:pending_zero_count]
+          acc[:pending_zero_count] = 0
+        elsif (stack.last == 1) && (i == 0)
+          acc[:pending_zero_count] = acc[:pending_zero_count] + 1
+        end
       end
 
       acc[:stack] = stack
-    end
+      acc
+    end[:zero_count]
   end
+
 end
