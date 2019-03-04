d91b94e6822d9fa7ddfb259e98d17bfefd2181d6
Warut Surapat <wsurapat@gilt.jp>
2019-03-04 17:45:05 +0900

    Clean up code.

--- water_trap/ruby/trap.rb
+++ water_trap/ruby/trap.rb
@@ -1,18 +1,18 @@
 # https://leetcode.com/problems/trapping-rain-water/
+
+# Idea:
+# Iterate through collection once and find the peaks.
+# Go through the list of peak, try to map one peak to the next peak that is equal or higher.
+# If there was no such peak, try to map the peak to the max peak of the remaining peak.
+# Remove the mapped peaks, then iterate through the remaining peaks.
+# Finally, calculate the volumne between peaks and sum up.
+
 class Trap
-  # @param {Integer[]} height
-  # @return {Integer}
   def trap(height)
     return 0 if height.length <= 2
     peaks = get_peaks(height)
 
-    peak_indexes = peaks[:peak_indexes]
-    peak_values = peaks[:peak_values]
-
-    data = peak_indexes.zip(peak_values)
-    data = data.map { |d| {index: d[0], value: d[1]}}
-
-    return 0 if data.length == 0
+    data = peaks[:peak_indexes].zip(peaks[:peak_values]).map { |d| {index: d[0], value: d[1]}}
 
     holes = []
     i = 0
@@ -24,22 +24,12 @@ class Trap
       search_space = data.slice(1, data.length).collect { |d| d[:value] }
       next_peak_index = next_peak(start_value, search_space) + 1
       end_index = data[next_peak_index][:index]
-      # puts "loop #{i}"
-      # puts "== data = #{data}"
-      # puts "== start_index = #{start_index}"
-      # puts "== start_value = #{start_value}"
-      # puts "== next_peak_index = #{next_peak_index}"
-      # puts "== end_index = #{end_index}"
       holes.push([start_index, end_index])
       data = data.slice(next_peak_index, data.length)
       i = i + 1
     end
-    # puts "holes = #{holes}"
 
-    holes.map do |range|
-      vol(height[range[0]..range[1]])
-    end.inject(0) { |acc, i| acc + i }
-
+    holes.map { |range| vol(height[range[0]..range[1]]) }.inject(0) { |acc, i| acc + i }
   end
 
   def next_peak(current_peak, peaks)
@@ -79,8 +69,6 @@ class Trap
 
         prev_trend = acc[:prev_trend]
 
-        # puts "index: #{index}, h: #{h}, next_h: #{height[index + 1]} trend: #{trend}, prev_trend: #{prev_trend}"
-
         if ((trend < 0) && (trend != prev_trend) || (prev_trend == 1 && trend == 0))
           # Found peak
           acc[:peak_indexes].push(index)
@@ -99,80 +87,5 @@ class Trap
       acc
     end
   end
+
 end
-
-
-# def get_peaks(height)
-#   height.inject({peak_indexes: [], peak_values: [], prev: 0, prev_trend: 0, index: 0}) do |acc, h|
-#       trend = (h - acc[:prev])
-#       trend = -1 if trend < 0
-#       trend = 1 if trend > 0
-#       prev_trend = acc[:prev_trend]
-#       # puts "get_peaks: trend = #{trend}, prev trend = #{prev_trend}"
-#       if (trend <= 0) && (trend != prev_trend) && (prev_trend != 0)
-#           # puts "get_peaks: at h = #{h}, figure out peak = #{acc[:prev]} at index = #{acc[:index] - 1}"
-#           acc[:peak_indexes].push(acc[:index] - 1)
-#           acc[:peak_values].push(acc[:prev])
-#       end
-
-#       acc[:prev] = h
-#       acc[:prev_trend] = trend
-#       acc[:index] = acc[:index] + 1
-
-#       # puts "get_peaks: end loop, acc[:index] = #{acc[:index]}, peaks.last = #{acc[:peak_indexes].last}"
-#       # puts "-------------"
-
-#       # if (acc[:index] == height.length) && (acc[:peak_indexes].length > 0) && (height[acc[:peak_indexes].last] <= h)
-#       if (acc[:index] == height.length) && (acc[:peak_indexes].length > 0) && (trend >= 0)
-#           # last item, keep it if it is ascending.
-#           # puts "-----------pick last -------"
-#           acc[:peak_indexes] = acc[:peak_indexes].push(acc[:index] - 1)
-#           acc[:peak_values] = acc[:peak_values].push(h)
-#       end
-#       acc
-#   end
-# end
-
-# def trap(height)
-#   return 0 if height.length < 2
-#   peaks = get_peaks(height)
-#   puts "========================"
-#   puts peaks
-#   peak_indexes = peaks[:peak_indexes]
-#   peak_values = peaks[:peak_values]
-
-
-#   res = peak_values.inject({holes: [], start_point: nil, start_point_index: nil, index: 0, current_hole: []}) do |acc, p|
-#       start_point = acc[:start_point]
-#       index = acc[:index]
-#       if start_point
-#           if p >= start_point
-#               acc[:current_hole] = acc[:current_hole].push(peak_indexes[index])
-#               acc[:holes] = acc[:holes].push(acc[:current_hole])
-#               acc[:start_point] = nil
-
-#               acc[:start_point] = peak_values[index]
-#               # acc[:start_point_index] = peak_indexes[index]]
-#               acc[:current_hole] = [peak_indexes[index]]
-#           end
-#       else
-#           acc[:start_point] = peak_values[index]
-#           # acc[:start_point_index] = peak_indexes[index]]
-#           acc[:current_hole] = [peak_indexes[index]]
-#       end
-#       acc[:index] = index + 1
-#       acc
-#   end
-#   puts "---------"
-#   puts res
-#   puts "---------"
-#   if res[:current_hole].length == 1
-#       res[:holes].push(res[:current_hole].push(peak_indexes[res[:index] - 1]))
-#   end
-#   puts res
-
-#   ranges = res[:holes]
-#   ranges.map do |range|
-#       vol(height[range[0]..range[1]])
-#   end.sum
-# end
