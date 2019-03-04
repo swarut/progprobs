d1b2c3fb3c9e14ae04add28a7fab5dd12dd8d736
Warut Surapat <wsurapat@gilt.jp>
2019-03-04 13:53:01 +0900

    Add another trap solution.

--- water_trap/ruby/another_trap.rb
+++ water_trap/ruby/another_trap.rb
@@ -1,5 +1,5 @@
+# Simple but not efficient.
 class AnotherTrap
-
   def count_inbetween_zero(arr)
     arr.inject({stack: [], pending_zero_count: 0, zero_count: 0}) do |acc, i|
       stack = acc[:stack]
@@ -20,4 +20,17 @@ class AnotherTrap
     end[:zero_count]
   end
 
+  def slice_pane(arr)
+    max = arr.max
+    (0...max).to_a.inject([]) do |acc, i|
+      acc.push(arr.map { |p| p - i > 0 ? 1 : 0 })
+    end
+  end
+
+  def trap(height)
+    return 0 if height.length < 2
+    slices = slice_pane(height)
+    vol_per_slice = slices.map { |s| count_inbetween_zero(s) }
+    vol_per_slice.inject(0) { |acc, i| acc + i}
+  end
 end
--- water_trap/ruby/spec/another_trap_spec.rb
+++ water_trap/ruby/spec/another_trap_spec.rb
@@ -14,4 +14,30 @@ describe AnotherTrap do
       expect(@trap.count_inbetween_zero([1, 0, 1, 0])).to eq(1)
     end
   end
+
+  describe 'slice_pane' do
+    it 'slices each element in array and return new array of slices' do
+      expect(@trap.slice_pane([0, 0, 0])).to eq []
+      expect(@trap.slice_pane([1, 1, 1])).to eq [[1, 1, 1]]
+      expect(@trap.slice_pane([2, 2, 2])).to eq [[1, 1, 1], [1, 1, 1]]
+      expect(@trap.slice_pane([2, 3, 2])).to eq [[1, 1, 1], [1, 1, 1], [0, 1, 0]]
+      expect(@trap.slice_pane([3, 2, 3])).to eq [[1, 1, 1], [1, 1, 1], [1, 0, 1]]
+      expect(@trap.slice_pane([4, 2, 3])).to eq [[1, 1, 1], [1, 1, 1], [1, 0, 1], [1, 0, 0]]
+      expect(@trap.slice_pane([4, 0, 0, 3])).to eq [[1, 0, 0, 1], [1, 0, 0, 1], [1, 0, 0 ,1], [1, 0, 0, 0]]
+    end
+  end
+
+  describe 'trap' do
+    it 'returns total trapped water volumne' do
+      expect(@trap.trap([0,1,0,2,1,0,1,3,2,1,2,6])).to eq 9
+      expect(@trap.trap([6,1,0,2,1,0,1,3,2,1,2,6])).to eq 47
+      expect(@trap.trap([1,7,8])).to eq 0
+      expect(@trap.trap([0,1,0,2,1,0,1,3,2,1,2,1])).to eq 6
+      expect(@trap.trap([4,2,3])).to eq 1
+      expect(@trap.trap([5,5,1,7,1,1,5,2,7,6])).to eq 23
+      expect(@trap.trap([4,9,4,5,3,2])).to eq 1
+      expect(@trap.trap([9,6,8,8,5,6,3])).to eq 3
+      expect(@trap.trap([9,6,8,8,5,6,3])).to eq 3
+    end
+  end
 end
