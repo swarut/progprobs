1cbd2dbd4d5e055ba7c7e51bb0756af054435167
Warut Surapat <wsurapat@gilt.jp>
2019-03-12 12:43:10 +0900

    First solution for longest palindromic.

--- longest_palindromic_string/ruby/longest_palindromic_string.rb
+++ longest_palindromic_string/ruby/longest_palindromic_string.rb
@@ -1,8 +1,49 @@
-https://leetcode.com/problems/longest-palindromic-substring/
+# https://leetcode.com/problems/longest-palindromic-substring/
 
-# Idea: Use stack for matching symmetric string.
 class LongestPalindromicString
 
-  def longest_palindrome(s)
+  def longest_palindrome(str)
+    str = str.split('')
+    return str.join if str.length <= 1
+    return (str.first == str.last ? str.join : str.first) if str.length == 2
+
+    max_match = nil
+    palindromic_ss = []
+    start_index = 1
+    end_index = str.length - 2
+    current_index = start_index
+
+    loop do
+      # palindromic_ss.push(palindrome_string(str, current_index))
+      if max_match == nil
+        max_match = palindrome_string(str, current_index)
+      else
+        p = palindrome_string(str, current_index)
+        max_match = p if p.length > max_match.length
+      end
+      current_index = current_index + 1
+      break if current_index == end_index
+    end
+
+    max_match
+  end
+
+  def palindrome_string(str, index)
+    times = 0
+    loop do
+      left_index = index - (1 * (times + 1))
+      right_index = index + (1 * (times + 1))
+      left = left_index >= 0 ? str[left_index] : nil
+      right = str[right_index]
+      # puts "str = #{str}, times = #{times}"
+      # puts "left-index #{left_index}, right-index #{right_index}"
+      # puts "left = #{left}, right = #{right}"
+      # puts "--------"
+
+      break if (left != right) || (left == nil) || (right == nil)
+      times = times + 1
+    end
+
+    str[(index - times)..(index + times)].join
   end
 end
--- longest_palindromic_string/ruby/spec/longest_palindromic_string_spec.rb
+++ longest_palindromic_string/ruby/spec/longest_palindromic_string_spec.rb
@@ -11,8 +11,22 @@ describe LongestPalindromicString do
       expect(@lps.longest_palindrome('aa')).to eq('aa')
       expect(@lps.longest_palindrome('ab')).to eq('a')
       expect(@lps.longest_palindrome('aabac')).to eq('aba')
-      expect(@lps.longest_palindrome('aabaaabaaa')).to eq('aaabaaa')
+      expect(@lps.longest_palindrome('aabaaabaaa')).to eq('aabaaabaa')
 
+    end
+  end
+
+  describe 'palindrome_string' do
+    it 'returns palindrom string' do
+      expect(@lps.palindrome_string('aab'.split(''), 1)).to eq('a')
+      expect(@lps.palindrome_string('aba'.split(''), 1)).to eq('aba')
+      expect(@lps.palindrome_string('abaa'.split(''), 1)).to eq('aba')
+      expect(@lps.palindrome_string('aabaa'.split(''), 1)).to eq('a')
+      expect(@lps.palindrome_string('aabaa'.split(''), 2)).to eq('aabaa')
+      expect(@lps.palindrome_string('caabaa'.split(''), 2)).to eq('a')
+      expect(@lps.palindrome_string('caabaa'.split(''), 3)).to eq('aabaa')
+      expect(@lps.palindrome_string('caabaad'.split(''), 3)).to eq('aabaa')
+      expect(@lps.palindrome_string('xcaabaadz'.split(''), 4)).to eq('aabaa')
     end
   end
 
