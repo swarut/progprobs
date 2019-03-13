a97aa93c20ab6cdbb7adeac5de88654a6e617f46
Warut Surapat <wsurapat@gilt.jp>
2019-03-13 18:24:39 +0900

    WIP

--- longest_palindromic_string/ruby/longest_palindromic_string.rb
+++ longest_palindromic_string/ruby/longest_palindromic_string.rb
@@ -29,47 +29,14 @@ class LongestPalindromicString
   def palindrome_string(str, index)
     left_index = index - 1
     right_index = index + 1
-    is_center_repeating = true
-    i = 0
-    all_length = str.length
+    is_left_center_repeating = true
+    is_right_center_repeating = true
+    str_length = str.length
     loop do
       left = left_index < 0 ? nil : str[left_index]
-      right = right_index > all_length ? nil : str[right_index]
-      is_center_repeating = false if (right != str[index]) && (right != nil)
-      puts "---- loop #{i}"
-      puts "-------left index = #{left_index}, value = #{left}"
-      puts "-------right index = #{right_index}, value = #{right}"
-      puts "-------is_center_repeating = #{is_center_repeating}"
+      right = right_index > str_length ? nil : str[right_index]
 
-      if (!is_center_repeating && (left != right)) || ((left == nil) || (right == nil))
-        left_index = left_index + 1 if !is_center_repeating
-        right_index = right_index - 1
-        break
-      # elsif (left == nil) || (right == nil)
-      #   puts "---------------------------------------------booooomm------------"
-      #   left_index = left_index + 1 if right != nil
-      #   right_index = right_index - 1 if left != nil
-      #   break
-      else
-        if is_center_repeating
-          right_index = right_index + 1
-        else
-          left_index = left_index - 1
-          right_index = right_index + 1
-        end
-      end
-      i = i + 1
+      is_left_center_repeating = false if
     end
-    puts "------final left_index = #{left_index}, right_index = #{right_index}"
-
-    if is_center_repeating
-      if str[left_index] == str[right_index]
-        return str[left_index..right_index].join
-      else
-        return str[(left_index + 1)..right_index].join
-      end
-    end
-
-    str[left_index..right_index].join
   end
 end
