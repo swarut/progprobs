3c52b976cbc0b685f24b73bd300d4742d88b7496
Warut Surapat <wsurapat@gilt.jp>
2019-03-15 11:15:10 +0900

    Finally made it.. palindrome .

--- longest_palindromic_string/ruby/longest_palindromic_string.rb
+++ longest_palindromic_string/ruby/longest_palindromic_string.rb
@@ -27,16 +27,57 @@ class LongestPalindromicString
   end
 
   def palindrome_string(str, index)
+    puts "-processing string #{str}, index #{index}"
     left_index = index - 1
     right_index = index + 1
     is_left_center_repeating = true
     is_right_center_repeating = true
     str_length = str.length
+    center = str[index]
     loop do
-      left = left_index < 0 ? nil : str[left_index]
-      right = right_index > str_length ? nil : str[right_index]
+      puts "------loop: left_index = #{left_index}, right_index = #{right_index}"
+      is_left_out_of_bound  = left_index < 0
+      is_right_out_of_bound = right_index >= str_length
+      is_left_right_out_of_bound = is_left_out_of_bound && is_right_out_of_bound
 
-      is_left_center_repeating = false if
+      left = is_left_out_of_bound ? nil : str[left_index]
+      right = is_right_out_of_bound ? nil : str[right_index]
+
+      is_left_center_repeating = false if (left != nil) && (left != center)
+      is_right_center_repeating = false if (right != nil) && (right != center)
+
+      if is_left_right_out_of_bound
+        left_index = left_index + 1
+        right_index = right_index - 1
+        break
+      elsif is_left_out_of_bound || is_right_out_of_bound
+        left_index = left_index + 1
+        right_index = right_index - 1
+        break
+      end
+
+      if (left != right)
+        if is_left_center_repeating
+          left_index = left_index - 1
+          # right_index = right_index - 1
+          # break
+        elsif is_right_center_repeating
+          right_index = right_index + 1
+          # left_index = left_index + 1
+          # break
+        else
+          left_index = left_index + 1
+          right_index = right_index - 1
+          break
+        end
+      else
+        left_index = left_index - 1
+        right_index = right_index + 1
+      end
+
     end
+    puts "left_index = #{left_index}, right_index=#{right_index}"
+    str[left_index..right_index].join
   end
+
 end
