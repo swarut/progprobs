1aa5afb6bb497e461402dca02aa982cd5e499b83
Warut Surapat <s.warut@gmail.com>
2019-03-19 03:32:58 +0900

    wip

--- single_element_in_sorted_array/ruby/single_element_in_sorted_array.rb
+++ single_element_in_sorted_array/ruby/single_element_in_sorted_array.rb
@@ -2,6 +2,18 @@
 
 class SingleElementInSortedArray
   def single_non_duplicate(nums)
+    if nums.length == 3
+      find_uniq(nums)
+    else
+      pivot = nums.length / 2
+      return nums[pivot] if (nums[pivot] != nums[pivot - 1]) && (nums[pivot] != nums[pivot + 1])
+      return single_non_duplicate(nums[0..(pivot - 1)]) if (nums[pivot] == nums[pivot + 1])
+      return single_non_duplicate(nums[(pivot + 1)...nums.length])
+    end
+  end
 
+  def find_uniq(array_of_3)
+    return array_of_3[2] if array_of_3[1] == array_of_3[0]
+    return array_of_3[0] if array_of_3[1] == array_of_3[2]
   end
 end
--- single_element_in_sorted_array/ruby/spec/single_element_in_sorted_array_spec.rb
+++ single_element_in_sorted_array/ruby/spec/single_element_in_sorted_array_spec.rb
@@ -7,7 +7,23 @@ describe SingleElementInSortedArray do
   describe 'single_non_duplicate' do
     context 'input with 3 elements' do
       it 'returns single element' do
-        expect(@seisa.single_non_duplicate([1,1,2])).to eq 2
+        expect(@seisa.single_non_duplicate([1, 1, 2])).to eq 2
+        expect(@seisa.single_non_duplicate([1, 2, 2])).to eq 1
+      end
+    end
+    context 'input with 5 elements' do
+      it 'returns single element' do
+        expect(@seisa.single_non_duplicate([1, 1, 2, 2, 3])).to eq 3
+        expect(@seisa.single_non_duplicate([1, 2, 2, 3, 3])).to eq 1
+        expect(@seisa.single_non_duplicate([1, 1, 3, 4, 4])).to eq 3
+      end
+    end
+    context 'input with 7 elements' do
+      it 'returns single element' do
+        expect(@seisa.single_non_duplicate([1, 1, 2, 2, 3, 3, 4])).to eq 4
+        expect(@seisa.single_non_duplicate([1, 2, 2, 3, 3, 4, 4])).to eq 1
+        expect(@seisa.single_non_duplicate([1, 1, 2, 3, 3, 4, 4])).to eq 2
+        expect(@seisa.single_non_duplicate([1, 1, 2, 2, 3, 4, 4])).to eq 3
       end
     end
   end
