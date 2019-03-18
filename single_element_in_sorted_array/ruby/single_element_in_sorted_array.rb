16085c0c68a59b167a50ad605b68fa6bd9c133b7
Warut Surapat <wsurapat@gilt.jp>
2019-03-18 19:58:03 +0900

    Add foundation for single element in sorted array problem.

---
+++ single_element_in_sorted_array/ruby/single_element_in_sorted_array.rb
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/single-element-in-a-sorted-array/
+
+class SingleElementInSortedArray
+  def single_non_duplicate(nums)
+
+  end
+end
---
+++ single_element_in_sorted_array/ruby/spec/single_element_in_sorted_array_spec.rb
@@ -0,0 +1,7 @@
+require './single_element_in_sorted_array'
+describe SingleElementInSortedArray do
+  before do
+    @seisa = SingleElementInSortedArray.new
+  end
+
+end
