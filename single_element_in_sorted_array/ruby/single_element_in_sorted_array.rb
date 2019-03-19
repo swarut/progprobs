5079948415889b95f91a706b11be09afacc74db0
Warut Surapat <wsurapat@gilt.jp>
2019-03-19 16:33:03 +0900

    Complete single element in sorted array.

--- single_element_in_sorted_array/ruby/single_element_in_sorted_array.rb
+++ single_element_in_sorted_array/ruby/single_element_in_sorted_array.rb
@@ -1,25 +1,34 @@
 # https://leetcode.com/problems/single-element-in-a-sorted-array/
 
 class SingleElementInSortedArray
-  def single_non_duplicate(nums)
-    puts "---processing #{nums}"
+  def single_non_duplicate(nums, pivot_lookup = [])
+    puts "---processing #{nums}, pivot_lookup = #{pivot_lookup}"
     if nums.length == 2
-      return nums[1] if nums[0] != nums[1]
+      return (nums - pivot_lookup).first
     elsif nums.length == 3
       find_uniq(nums)
     else
-      pivot = nums.length / 2
-      new_length = nums.length - 1
+      pivot_index = nums.length / 2
+      new_length = (nums.length - 1)/2
+      pivot_lookup.push(nums[pivot_index])
 
-      return nums[pivot] if (nums[pivot] != nums[pivot - 1]) && (nums[pivot] != nums[pivot + 1])
+      return nums[pivot_index] if (nums[pivot_index] != nums[pivot_index - 1]) && (nums[pivot_index] != nums[pivot_index + 1])
 
-      if new_length % 2 == 0
-        return single_non_duplicate(nums[(pivot + 1)...nums.length]) if (nums[pivot] == nums[pivot + 1])
-        return single_non_duplicate(nums[0..(pivot - 1)])
-      else
-        return single_non_duplicate(nums[0..(pivot - 1)]) if (nums[pivot] == nums[pivot + 1])
-        return single_non_duplicate(nums[(pivot + 1)...nums.length])
+      if (nums[pivot_index] == nums[pivot_index + 1])
+        if (nums.length - pivot_index) % 2 == 1
+          return single_non_duplicate(nums[pivot_index...nums.length], pivot_lookup)
+        else
+          return single_non_duplicate(nums[0..(pivot_index - 1)], pivot_lookup)
+        end
+
+      elsif (nums[pivot_index] == nums[pivot_index -1])
+        if (pivot_index + 1) % 2 == 1
+          return single_non_duplicate(nums[0..pivot_index], pivot_lookup)
+        else
+          return single_non_duplicate(nums[(pivot_index + 1)..nums.length], pivot_lookup)
+        end
       end
+
     end
   end
 
