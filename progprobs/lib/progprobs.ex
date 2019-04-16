24a905ed939605d72f1cdce819ced1a77d9d02fd
Warut Surapat <wsurapat@gilt.jp>
2019-04-16 19:59:36 +0900

    WIP

---
+++ progprobs/lib/progprobs.ex
@@ -0,0 +1,5 @@
+defmodule Progprobs.CLI do
+  def main(_args) do
+    IO.puts "Hello"
+  end
+end
---
+++ progprobs/test/progprobs_test.exs
@@ -0,0 +1,8 @@
+defmodule ProgprobsTest do
+  use ExUnit.Case
+  doctest Progprobs
+
+  test "greets the world" do
+    assert Progprobs.hello() == :world
+  end
+end
