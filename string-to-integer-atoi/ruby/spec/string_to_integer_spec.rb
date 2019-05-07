f327a1ce850d8674db9773dc0319e708ae9ccaa5
Warut Surapat <s.warut@gmail.com>
2019-05-07 22:50:37 +0900

    Remove bad problem.

--- string-to-integer-atoi/ruby/spec/string_to_integer_spec.rb
+++ string-to-integer-atoi/ruby/spec/string_to_integer_spec.rb
@@ -1,17 +1,17 @@
 require './string_to_integer'
 describe StringToInteger do
-  # before do
-  #   @lstnr = LongestSubstringNoRepeat.new
-  # end
+  before do
+    @sti = StringToInteger.new
 
-  # describe 'length_of_longest_substring' do
-  #   it 'returns correct length' do
-  #     expect(@lstnr.length_of_longest_substring("dvdf")).to eq(3)
-  #     expect(@lstnr.length_of_longest_substring("aaaabadefcdheeeeabcdefghij")).to eq(10)
-  #     expect(@lstnr.length_of_longest_substring("abcabcbb")).to eq(3)
-  #     expect(@lstnr.length_of_longest_substring("abba")).to eq(2)
-  #     expect(@lstnr.length_of_longest_substring("cfxhowcgimmupeovulclalqcghiuaphcwfkn")).to eq(10)
-  #   end
-  # end
+  end
+  describe 'string_to_integer' do
+    it 'returns correct length' do
+      expect(@lstnr.length_of_longest_substring("dvdf")).to eq(3)
+      expect(@lstnr.length_of_longest_substring("aaaabadefcdheeeeabcdefghij")).to eq(10)
+      expect(@lstnr.length_of_longest_substring("abcabcbb")).to eq(3)
+      expect(@lstnr.length_of_longest_substring("abba")).to eq(2)
+      expect(@lstnr.length_of_longest_substring("cfxhowcgimmupeovulclalqcghiuaphcwfkn")).to eq(10)
+    end
+  end
 
 end
--- string-to-integer-atoi/ruby/string_to_integer.rb
+++ string-to-integer-atoi/ruby/string_to_integer.rb
@@ -1,27 +1,6 @@
 # https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
-class LongestSubstringNoRepeat
-  def length_of_longest_substring(s)
-    found_chars = {}
-
-    current_substring_length = 0
-    current_start_index = 0
-    max_substring_length = 0
-    index = 0
-
-    s.split('').each do |st|
-      if (found_chars[st] != nil) && (found_chars[st] >= current_start_index)
-          max_substring_length = [current_substring_length, max_substring_length].max
-          current_substring_length = (index - found_chars[st])
-          current_start_index = found_chars[st]
-          found_chars[st] = index
-      else
-          found_chars[st] = index
-          current_substring_length = current_substring_length + 1
-      end
-
-      index = index + 1
-    end
-    [current_substring_length, max_substring_length].max
+class StringToInteger
+  def string_to_integer(str)
   end
 end
