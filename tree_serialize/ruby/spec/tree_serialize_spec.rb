483646821e6d4f3bb07786160b4967e5dced6d52
Warut Surapat <wsurapat@gilt.jp>
2019-10-04 15:54:55 +0900

    Update spec.

--- tree_serialize/ruby/spec/tree_serialize_spec.rb
+++ tree_serialize/ruby/spec/tree_serialize_spec.rb
@@ -21,3 +21,16 @@ def build_simple_tree
   t1.add_left(t2)
   t1.add_right(t3)
 end
+
+def build_simple_tree2
+  t1 = Tree.new(1)
+  t2 = Tree.new(2)
+  t3 = Tree.new(3)
+  t4 = Tree.new(4)
+  t5 = Tree.new(5)
+  t6 = Tree.new(6)
+  t7 = Tree.new(7)
+
+  t1.add_left(t2)
+  t1.add_right(t3)
+end
