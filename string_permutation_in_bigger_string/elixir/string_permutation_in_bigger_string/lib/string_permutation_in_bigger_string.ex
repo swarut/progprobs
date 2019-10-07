c3b32d8745ac836c36d5bc4511b67790d5cdd4e4
Warut Surapat <wsurapat@gilt.jp>
2019-10-07 16:17:50 +0900

    Add elixir folder.

---
+++ string_permutation_in_bigger_string/elixir/string_permutation_in_bigger_string/lib/string_permutation_in_bigger_string.ex
@@ -0,0 +1,18 @@
+defmodule StringPermutationInBiggerString do
+  @moduledoc """
+  Documentation for StringPermutationInBiggerString.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> StringPermutationInBiggerString.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ string_permutation_in_bigger_string/elixir/string_permutation_in_bigger_string/test/string_permutation_in_bigger_string_test.exs
@@ -0,0 +1,8 @@
+defmodule StringPermutationInBiggerStringTest do
+  use ExUnit.Case
+  doctest StringPermutationInBiggerString
+
+  test "greets the world" do
+    assert StringPermutationInBiggerString.hello() == :world
+  end
+end
