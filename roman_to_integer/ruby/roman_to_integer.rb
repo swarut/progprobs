67337df31b214504a5551bfaf7297abd8ee712f2
Warut Surapat <s.warut@gmail.com>
2019-11-05 22:57:33 +0900

    Update roman to integer.

--- roman_to_integer/ruby/roman_to_integer.rb
+++ roman_to_integer/ruby/roman_to_integer.rb
@@ -14,38 +14,20 @@ class RomanToInteger
   end
 
   def self.convert(roman, acc, hold, last)
-    puts "ROMAN = #{roman}"
-    puts "ACC = #{acc}"
-    puts "HOLD = #{hold}"
-    puts "LAST = #{last}"
-    puts "-----"
     return acc + hold if roman.length == 0
 
-    # recent_token = roman[0]
     recent_token = roman[roman.length - 1]
     recent_value = LOOKUP[recent_token]
     last_value = LOOKUP[last]
 
-    if (recent_token == last) || !last_value
-      # Same token, just keep the value and read next
+    if !last_value || (recent_token == last) || (recent_value > last_value)
       hold = hold + recent_value
-    elsif recent_value > last_value
-      # The current token is greater, apply all value held so far
-      acc = acc - hold
-      acc = acc + recent_value
-      hold = 0
     elsif recent_value < last_value
-      hold = hold + recent_value
-      # acc = acc + hold
-      # hold = 0
+      acc = acc + hold
+      acc = acc - recent_value
+      hold = 0
+
     end
-    puts "RECENT TOKEN = #{recent_token}"
-    puts "RECENT VALUE = #{recent_value}"
-    puts "LAST VALUE = #{last_value}"
-    puts "UPDATED HOLD = #{hold}"
-    puts "UPDATED ACC = #{acc}"
-    puts "======================="
-    # convert(roman[1..roman.length], acc, hold, recent_token)
     convert(roman[0...(roman.length - 1)], acc, hold, recent_token)
   end
 end
