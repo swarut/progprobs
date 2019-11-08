ca8a6e2dbed860765549499c004757553bba94cb
Warut Surapat <wsurapat@gilt.jp>
2019-11-08 10:41:53 +0900

    Finish basic calculator.

--- basic_calculator/ruby/basic_calculator.rb
+++ basic_calculator/ruby/basic_calculator.rb
@@ -4,35 +4,24 @@ class BasicCalculator
   OPERATORS = ['+', '-']
 
   def self.cal(s)
-    result = s.gsub(' ', '').split(/(\))|(\()|(\+)|(\-)/).reject{ |d| d.empty? }.inject({ result: 0, stack: [], operants: []}) do |acc, i|
-      # puts "READ ::: #{i}"
-      # puts "Before: #{acc}"
+    result = s.gsub(' ', '').split(/(\))|(\()|(\+)|(\-)/).reject{ |d| d.empty? }.inject({stack: [], operants: []}) do |acc, i|
       if i == ')'
-        # puts "---- compute within ()"
         i = acc[:stack].pop
         acc[:stack].pop
       end
 
       if !acc[:operants].empty? && (i != '(') && (acc[:stack].last != '(') && (!OPERATORS.include?(i))
-        # puts "---- compute"
         result = compute(acc[:stack].pop.to_i, i.to_i, acc[:operants].pop)
         acc[:stack].push result
       else
         if OPERATORS.include?(i)
-          # puts "---- add operant"
           acc[:operants].push(i)
-        elsif i == '('
-          # puts "---- add ("
-          acc[:stack].push i
         else
-          # puts "---- add #{i} to stack"
           acc[:stack].push(i)
         end
       end
-      # puts "After: #{acc}"
-      # puts "===========================\n\n\n"
       acc
-    end[:stack].first
+    end[:stack].first.to_i
 
   end
 
