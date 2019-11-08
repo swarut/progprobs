5e5f5b9567cd38df1eac925264eb00dc18140288
Warut Surapat <wsurapat@gilt.jp>
2019-11-08 09:38:13 +0900

    Basic cal without () handler.

--- basic_calculator/ruby/basic_calculator.rb
+++ basic_calculator/ruby/basic_calculator.rb
@@ -1,15 +1,33 @@
 # https://leetcode.com/problems/basic-calculator/
 class BasicCalculator
 
-  MATCHERS = {
-
-  }
+  MATCHERS = ['(', ')']
   OPERATORS = ['+', '-']
 
-  def cal(s)
-    s.gsub(' ', '').split('').inject({ result: 0, stack: [], groups: [], last: nil}) do |acc, i|
-      acc[:stack].push(i)
+  def self.cal(s)
+    result = s.gsub(' ', '').split(/(\+)|(\-)/).inject({ result: 0, stack: [], operants: []}) do |acc, i|
+      if !acc[:operants].empty?
+        result = compute(acc[:stack].pop.to_i, i.to_i, acc[:operants].pop)
+        acc[:stack].push result
+      else
+        if OPERATORS.include?(i)
+          acc[:operants].push(i)
+        else
+          acc[:stack].push(i)
+        end
+      end
+      acc
+    end[:stack].first
 
+  end
+
+  def self.compute(a, b, operant)
+    case operant
+    when '+'
+      a + b
+    when '-'
+      a - b
     end
   end
+
 end
--- basic_calculator/ruby/spec/basic_calculator_spec.rb
+++ basic_calculator/ruby/spec/basic_calculator_spec.rb
@@ -7,15 +7,25 @@ describe BasicCalculator do
         expect(BasicCalculator.cal('1 + 1')).to eq(2)
         expect(BasicCalculator.cal('1 +1')).to eq(2)
         expect(BasicCalculator.cal('1+ 1')).to eq(2)
-      end
-    end
-    context 'with parentheses' do
-      it 'calculates expression' do
-        expect(BasicCalculator.cal('(1+1)')).to eq(2)
-        expect(BasicCalculator.cal('(1 + 1) + 2')).to eq(4)
+        expect(BasicCalculator.cal('1-1')).to eq(0)
+        expect(BasicCalculator.cal('1 - 1')).to eq(0)
+        expect(BasicCalculator.cal('1 -1')).to eq(0)
+        expect(BasicCalculator.cal('1- 1')).to eq(0)
 
+        expect(BasicCalculator.cal('1 + 1 +1')).to eq(3)
+        expect(BasicCalculator.cal('1 + 1 + 1')).to eq(3)
+        expect(BasicCalculator.cal('1 + 1 - 1')).to eq(1)
+
+        expect(BasicCalculator.cal('1 + 10 - 2 - 4')).to eq(5)
       end
     end
+    # context 'with parentheses' do
+    #   it 'calculates expression' do
+    #     expect(BasicCalculator.cal('(1+1)')).to eq(2)
+    #     expect(BasicCalculator.cal('(1 + 1) + 2')).to eq(4)
+
+    #   end
+    # end
   end
 
 end
