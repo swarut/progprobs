f8b3e71cd5593e2e873e9623a7e1d6141d717ceb
Warut Surapat <s.warut@gmail.com>
2020-01-01 11:25:25 +0900

    Add solution for remove duplicated from sorted array.

--- remove_duplicated_from_sorted_array/ruby/remove_duplicated_from_sorted_array.rb
+++ remove_duplicated_from_sorted_array/ruby/remove_duplicated_from_sorted_array.rb
@@ -1,4 +1,18 @@
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 class RemoveDuplicatedFromSortedArray
-  def remove_duplicated_from_sorted_array
+  def solve(input)
+    i = 0
+    last = nil
+    new_length = 0
+    loop do
+      val = input[i]
+      break if !val
+      if val != last
+        new_length += 1
+      end
+      last = input[i]
+      i += 1
+    end
+    new_length
   end
 end
--- remove_duplicated_from_sorted_array/ruby/spec/remove_duplicated_from_sorted_array_spec.rb
+++ remove_duplicated_from_sorted_array/ruby/spec/remove_duplicated_from_sorted_array_spec.rb
@@ -4,9 +4,23 @@ describe RemoveDuplicatedFromSortedArray do
     @remove_duplicated_from_sorted_array = RemoveDuplicatedFromSortedArray.new
   end
 
-  describe '' do
-    it '' do
-      expect().to eq()
+  describe 'solve' do
+    it 'solves' do
+      input = [1, 2, 3]
+      expect(@remove_duplicated_from_sorted_array.solve(input)).to eq(3)
+
+      input = [1, 1, 2, 3]
+      expect(@remove_duplicated_from_sorted_array.solve(input)).to eq(3)
+      puts "input = #{input}"
+
+      # input = [1, 2, 2, 3]
+      # expect(@remove_duplicated_from_sorted_array.solve(input)).to eq(3)
+
+      # input = [1, 2, 3, 3]
+      # expect(@remove_duplicated_from_sorted_array.solve(input)).to eq(3)
+
+      # input = [1, 1, 1, 1]
+      # expect(@remove_duplicated_from_sorted_array.solve(input)).to eq(1)
     end
   end
 
